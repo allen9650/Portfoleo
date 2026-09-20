1178b40a291614f0415d30533f3afa9150f5ccc1
ahsan <arkolachi190@gmail.com>
2026-09-20 19:01:40 +0500

    chore: update tab title to only Ahsan Raza

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -6,7 +6,7 @@ export default function ScrollToTop() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    document.title = 'Ahsan Raza | IT Enthusiast';
+    document.title = 'Ahsan Raza';
   }, [pathname]);
 
   return null;
